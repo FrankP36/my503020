126c8a3dc7bbdae6b9256fd9aef856c4d38c704b
FrankP36 <138867605+FrankP36@users.noreply.github.com>
2025-11-14 21:09:04 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,13 @@
-// Import Firebase SDK
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-// Your Firebase Config
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDtLg-93_BcASTg4E5oniGlMkGXQOAo94k",
   authDomain: "my503020.firebaseapp.com",
   projectId: "my503020",
-  storageBucket: "my503020.appspot.com",
+  storageBucket: "my503020.firebasestorage.app",
   messagingSenderId: "612049785066",
   appId: "1:612049785066:web:ba85e8e2af24a05b894793",
   measurementId: "G-N31DMC43S9"
@@ -16,4 +16,3 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
