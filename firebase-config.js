436ef060c2a54843a489000d8c469627d60b90aa
FrankP36 <138867605+FrankP36@users.noreply.github.com>
2025-11-14 15:38:11 +0300

    Add Firebase configuration and initialization

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js";
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDtLg-93_BcASTg4E5oniGlMkGXQOAo94k",
+  authDomain: "my503020.firebaseapp.com",
+  projectId: "my503020",
+  storageBucket: "my503020.appspot.com",
+  messagingSenderId: "612049785066",
+  appId: "1:612049785066:web:ba85e8e2af24a05b894793",
+  measurementId: "G-N31DMC43S9"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
